7694bfd777a2c14978857ff47e5d218d60cd0e95
Denys Dovzhenko <dovzdenis@gmail.com>
2024-02-05 10:53:37 +0200

    added expo image

--- src/components/HomeBackground/HomeBackground.tsx
+++ src/components/HomeBackground/HomeBackground.tsx
@@ -1,21 +1,34 @@
 import { Canvas, LinearGradient, Rect, vec } from "@shopify/react-native-skia";
+import { ImageBackground } from "expo-image";
 import { StyleSheet, useWindowDimensions } from "react-native";
 
 export default function HomeBackground() {
   const { width, height } = useWindowDimensions();
   return (
-    <Canvas style={{ width, height }}>
-      <Rect x={0} y={0} width={width} height={height}>
-        <LinearGradient start={vec(0, 0)} end={vec(width, height)} colors={["blue", "yellow"]} />
-      </Rect>
-    </Canvas>
+    <>
+      <Canvas style={styles.canvas}>
+        <Rect x={0} y={0} width={width} height={height}>
+          <LinearGradient
+            start={vec(0, 0)}
+            end={vec(width, height)}
+            colors={["#2E335A", "#1C1B33"]}
+          />
+        </Rect>
+      </Canvas>
+      <ImageBackground
+        source={require("@/assets/home/Background.png")}
+        contentFit="cover"
+        style={styles.image}
+      />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  canvas: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  image: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
   },
 });
