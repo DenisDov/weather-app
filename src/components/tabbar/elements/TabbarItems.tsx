1ab42560b432548fd8257ba946e218fe8b7e999b
Denys Dovzhenko <dovzdenis@gmail.com>
2024-02-06 13:33:15 +0200

    add TrapezoidBackground

--- src/components/tabbar/elements/TabbarItems.tsx
+++ src/components/tabbar/elements/TabbarItems.tsx
@@ -1,9 +1,15 @@
-import { Text, View, StyleSheet } from "react-native";
+import { View } from "react-native";
 
+import TrapezoidBackground from "./TrapezoidBackground";
 import ListIcon from "../icons/ListIcon";
 import MapIcon from "../icons/MapIcon";
 
+import useApplicationDimensions from "@/hooks/useApplicationDimensions";
+
 export default function TabbarItems() {
+  const { width, height } = useApplicationDimensions();
+  const trapezoidWidth = width * 0.709;
+  const trapezoidHeight = height * 0.123;
   return (
     <View
       style={{
@@ -11,13 +17,11 @@ export default function TabbarItems() {
         flexDirection: "row",
         alignItems: "center",
         justifyContent: "space-between",
-        backgroundColor: "red",
         paddingHorizontal: 32,
       }}>
       <MapIcon />
+      <TrapezoidBackground width={trapezoidWidth} height={trapezoidHeight} />
       <ListIcon />
     </View>
   );
 }
-
-const styles = StyleSheet.create({});
