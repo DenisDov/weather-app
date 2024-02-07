8df340a1fc403e5b7f0f943c6a538ea6d05d1860
Denys Dovzhenko <dovzdenis@gmail.com>
2024-02-07 16:33:25 +0200

    add ForecastCapsule

--- src/components/sheet/ForecastSheet.tsx
+++ src/components/sheet/ForecastSheet.tsx
@@ -1,17 +1,22 @@
 import BottomSheet from "@gorhom/bottom-sheet";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Platform } from "react-native";
 
 import ForecastSheetBackground from "./ForecastSheetBackground";
 import ForecastControl from "./elements/ForecastControl";
 import Separator from "./elements/Separator";
+import ForecastCapsule from "../forecast/ForecastCapsule";
 
 import useApplicationDimensions from "@/hooks/useApplicationDimensions";
+import { hourly } from "@/lib/data/ForecastData";
 
 export default function ForecastSheet() {
   const { width, height } = useApplicationDimensions();
-  const snapPoints = ["39%", "84%"];
+  const snapPoints = ["38%", "83%"];
   const firstSnapPoint = height * (parseFloat(snapPoints[0]) / 100);
   const cornerRadius = 44;
+  const capsuleRadius = 30;
+  const capsuleHeight = height * 0.17;
+  const capsuleWidth = width * 0.15;
   return (
     <BottomSheet
       snapPoints={snapPoints}
@@ -28,7 +33,13 @@ export default function ForecastSheet() {
         />
       )}>
       <ForecastControl />
-      <Separator width={width} height={3} />
+      <Separator width={width} height={1} />
+      <ForecastCapsule
+        width={capsuleWidth}
+        height={capsuleHeight}
+        radius={capsuleRadius}
+        forecast={hourly[0]}
+      />
     </BottomSheet>
   );
 }
