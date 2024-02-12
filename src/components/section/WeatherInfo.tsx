70c634883b40fca4dfe122b7c3e3bb8142b3acd5
Denys Dovzhenko <dovzdenis@gmail.com>
2024-02-12 12:22:42 +0200

    animate WeatherInfo

--- src/components/section/WeatherInfo.tsx
+++ src/components/section/WeatherInfo.tsx
@@ -1,6 +1,13 @@
 import { Text, View, StyleSheet } from "react-native";
+import Animated, {
+  Extrapolation,
+  interpolate,
+  interpolateColor,
+  useAnimatedStyle,
+} from "react-native-reanimated";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
+import { useForecastSheetPosition } from "@/context/ForecastSheetContext";
 import { DEGREE_SYMBOL } from "@/lib/Constants";
 import { Weather } from "@/models/Weather";
 
@@ -11,21 +18,53 @@ interface WeatherInfoProps {
 export default function WeatherInfo({ weather }: WeatherInfoProps) {
   const { city, temperature, condition, high, low } = weather;
   const { top } = useSafeAreaInsets();
-  const weatherInfomMargin = top + 51;
+  const topMargin = 51;
+  const weatherInfoMargin = top + topMargin;
+  const animatedPosition = useForecastSheetPosition();
+  const animatedViewStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateY: interpolate(
+            animatedPosition.value,
+            [0, 1],
+            [0, -topMargin],
+            Extrapolation.CLAMP,
+          ),
+        },
+      ],
+    };
+  });
+  const animatedTempTxtStyles = useAnimatedStyle(() => {
+    const fontFamily = animatedPosition.value > 0.5 ? "SF-Semibold" : "SF-Thin";
+    return {
+      fontFamily,
+      opacity: interpolate(animatedPosition.value, [0, 0.5, 1], [1, 0, 1]),
+      fontSize: interpolate(animatedPosition.value, [0, 1], [96, 20]),
+      lineHeight: interpolate(animatedPosition.value, [0, 1], [96, 20]),
+      color: interpolateColor(animatedPosition.value, [0, 1], ["white", "rgba(235, 235,245,0.6)"]),
+    };
+  });
+  const animatedMinMaxTxtStyles = useAnimatedStyle(() => {
+    return {
+      opacity: interpolate(animatedPosition.value, [0, 0.5], [1, 0]),
+    };
+  });
   return (
-    <View style={{ alignItems: "center", marginTop: weatherInfomMargin }}>
-      <Text style={styles.cityText}>{city}</Text>
-      <Text style={styles.temperatureText}>
+    <Animated.View
+      style={[{ alignItems: "center", marginTop: weatherInfoMargin }, animatedViewStyle]}>
+      <Animated.Text style={styles.cityText}>{city}</Animated.Text>
+      <Animated.Text style={[styles.temperatureText, animatedTempTxtStyles]}>
         {temperature}
         {DEGREE_SYMBOL}
-      </Text>
-      <Text style={styles.conditionText}>{condition}</Text>
-      <Text style={styles.minMaxText}>
+      </Animated.Text>
+      <Animated.Text style={styles.conditionText}>{condition}</Animated.Text>
+      <Animated.Text style={[styles.minMaxText, animatedMinMaxTxtStyles]}>
         H:{high}
         {DEGREE_SYMBOL} L:{low}
         {DEGREE_SYMBOL}
-      </Text>
-    </View>
+      </Animated.Text>
+    </Animated.View>
   );
 }
 
